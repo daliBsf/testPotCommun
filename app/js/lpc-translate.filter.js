a118f88b143344f29cfc4161a889b522964e25c3
dali <boussoffaramedali@gmail.com>
2018-10-23 14:25:32 +0100

    fix bugs

--- app/js/lpc-translate.filter.js
+++ app/js/lpc-translate.filter.js
@@ -3,15 +3,16 @@
 angular.module('LpcWebTest2017')
     .filter('lpcTranslate',['LpcTranslateService','$rootScope',function(LpcTranslateService,$rootScope){
         //TODO
-        LpcTranslateService.loadProperties().then(res=> { 
+        var promise = LpcTranslateService.loadProperties()
+        promise.then(res=> {
             $rootScope.properties  = res.data; 
         })
+        
         var filter = function(key,locale){
             
             if (! angular.isDefined($rootScope.properties))
                 return ""; 
-            return $rootScope.properties[locale][key]
-          
+            return $rootScope.properties[locale][key] 
         }
         return filter;
     }]);
